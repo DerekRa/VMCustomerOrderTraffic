63ca19a09b1d57dddafa9d5e46d08c291db678dc
Kenneth M <armstrong17.km@gmail.com>
2021-09-07 11:36:28 +0800

    my initial commit

---
+++ src/main/java/com/km/vmcustomerordertraffic/VmCustomerOrderTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.km.vmcustomerordertraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmCustomerOrderTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmCustomerOrderTrafficApplication.class, args);
+	}
+
+}
